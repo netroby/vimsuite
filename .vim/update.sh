6a015ea1beccf2516fd0395b18848fd3e5af4085
ZhiFeng Hu <hzfeng@vip.deyi.com>
2014-03-21 12:43:04 +0800

    Update the bundle and set whitespace instead tab

--- .vim/update.sh
+++ .vim/update.sh
@@ -5,10 +5,8 @@ do
 	echo "Proccess ${dir} ......"
 	cd $dir 
 	git config core.fileMode false && git reset --hard &&  git pull --rebase
-	\cp -rf ./* /usr/local/src/vim-zip/vim/vim73/
 	cd ..
 	echo "Done ......"
 done
-\cp -rf /usr/local/share/vim/vim74a/* /usr/local/src/vim-zip/vim/vim73/
 
 
