2cec8a3ad9ac0d97effe519f9e1ea4fce6969219
ZhiFeng Hu <zf@ancientrocklab.com>
2014-05-30 07:56:40 +0800

    Merge two update script to one

--- update.sh
+++ update.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 cd  ~/vimsuite && git pull --rebase && git submodule update --init
 cp -rvf .vi* ~/
-vim
+cd .vim/bundle
+for dir in $(ls -d */)
+do
+	echo "Proccess ${dir} ......"
+	cd $dir 
+	git config core.fileMode false && git checkout master  && git reset --hard &&  git pull --rebase
+	cd ..
+	echo "Done ......"
+done
+
+
